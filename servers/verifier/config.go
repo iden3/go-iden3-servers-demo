a695bf9520f3d7150ad9a528cb35ad7c371ecee0
a_bennassar <arnaubennassar5@gmail.com>
2020-05-13 15:58:35 +0200

    Add static file server endpoint

--- servers/verifier/config.go
+++ servers/verifier/config.go
@@ -2,8 +2,15 @@ package main
 
 import "github.com/iden3/go-iden3-servers/config"
 
+// Config provides the necessary set up data to run the server
 type Config struct {
-	Server    config.Server
-	Contracts config.Contracts
-	Web3      config.Web3
+	Server          config.Server
+	Contracts       config.Contracts
+	Web3            config.Web3
+	StaticResources StaticResources
+}
+
+// StaticResources holds the necessary information to serve static files
+type StaticResources struct {
+	Path string
 }
